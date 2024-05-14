0a6d8c54a789e0b755e853658d448c954dfc53b4
sumit200405 <116437742+sumit200405@users.noreply.github.com>
2024-05-15 00:09:09 +0530

    contact us page

--- public/script.js
+++ public/script.js
@@ -2,11 +2,11 @@ const toggleButton = document.querySelector(".toggle-button")
 const toggleButtonIcon = document.querySelector(".toggle-button i")
 const dropdownMenu = document.querySelector(".dropdown-menu")
 
-toggleButton.onclick = function(){
-dropdownMenu.classList.toggle('open')
-const isOpen = dropdownMenu.classList.contains('open')
+toggleButton.onclick = function () {
+    dropdownMenu.classList.toggle('open')
+    const isOpen = dropdownMenu.classList.contains('open')
 
-toggleButtonIcon.className = isOpen
-? 'fa-solid fa-xmark'
-: 'fa-solid fa-bars'
+    toggleButtonIcon.className = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
 }
